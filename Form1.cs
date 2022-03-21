2ba17a587e314900664d2291c2b04fe989426958
NICO <NICO@DESKTOP-4ONFR90>
2022-03-21 11:18:06 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Praktikum_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton1.ForeColor = Color.Red;
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton2.ForeColor = Color.Blue;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
